fb9930e3a579ef9d29c8ce03a8e59e1d28472fe7
Suyog Waghere <suyogwaghere99@gmail.com>
2023-10-26 23:07:58 -0400

    jwt added to secure the routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
+const { verifyToken } = require("./controller/userController"); // Import the verifyToken middleware
 const sequelize = require("./connection/db"); // Import your Sequelize instance
 const User = require("./models/User"); // Import your User model
 app.use(express.json());
@@ -26,12 +27,12 @@ const passwordStrengthRoute = require("./routes/passwordStrengthRoute");
 const userRoute = require("./routes/userRoute");
 
 // Use the routes
-app.use("/api/ping", pingRoute);
-app.use("/api/nmap", nmapRoute);
-app.use("/api/nslookup", nslookupRoute);
-app.use("/api/whois", whoisRoute);
+app.use("/api/ping", verifyToken, pingRoute);
+app.use("/api/nmap", verifyToken, nmapRoute);
+app.use("/api/nslookup", verifyToken, nslookupRoute);
+app.use("/api/whois", verifyToken, whoisRoute);
 app.use("/api/whatweb", whatwebRoute);
-app.use("/api/password-strength", passwordStrengthRoute);
+app.use("/api/password-strength", verifyToken, passwordStrengthRoute);
 app.use("/api/user", userRoute);
 
 app.listen(port, () => {
